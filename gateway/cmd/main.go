154c836bf24f1daf7cedfd7143c883842fd14d1c
stepa <stipochka@stepas-MacBook-Air.local>
2025-09-21 20:26:58 +0300

    feat: added proper connect to broker

--- gateway/cmd/main.go
+++ gateway/cmd/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"context"
+	"gateway/internal/handler"
+	"gateway/internal/producer"
+	"gateway/internal/service"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
+)
+
+const (
+	addr            = "127.0.0.1:8080"
+	shutdownTimeout = time.Second * 5
 )
 
 func main() {
@@ -12,6 +26,62 @@ func main() {
 		"time", time.Now,
 	).Info("Started api gateway")
 
-	// INIT producer
+	err := godotenv.Load()
+	if err != nil {
+		log.WithField(
+			"error", err.Error(),
+		).Fatal("failed to load .env")
+	}
+
+	natsAddr := os.Getenv("NATS_ADDR")
+	if natsAddr == "" {
+		log.Fatal("empty nats addr")
+	}
+
+	prod, err := producer.NewNatsProducer(natsAddr)
+	if err != nil {
+		log.WithField(
+			"error", err.Error(),
+		).Fatal("failed to connect to broker")
+	}
+
+	useCase := service.NewTaskSender(prod)
+
+	handler := handler.NewHandler(useCase)
+
+	router := handler.InitRoute()
+
+	serverAddr := os.Getenv("HTTP_SERVER")
+	if serverAddr == "" {
+		serverAddr = addr
+	}
+
+	srv := &http.Server{
+		Addr:    serverAddr,
+		Handler: router,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.WithField("error", err.Error()).Error("error while running server")
+		}
+	}()
+
+	<-done
+	log.Info("stopping server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.WithField("error", err.Error()).Error("error while stopping server")
+
+		return
+	}
+
+	log.Info("service stopped")
 
 }
--- gateway/go.mod
+++ gateway/go.mod
@@ -11,6 +11,7 @@ require (
 
 require (
 	github.com/ajg/form v1.5.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/nats-io/nats.go v1.45.0 // indirect
 	github.com/nats-io/nkeys v0.4.11 // indirect
